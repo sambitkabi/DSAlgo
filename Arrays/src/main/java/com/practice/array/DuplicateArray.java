fe8917e2c922d2e2eb4d0edc479b0bccd1db06b4
sambit kabi <sambit.kabi@gmail.com>
2022-01-23 00:23:38 +0530

    First commit

---
+++ Arrays/src/main/java/com/practice/array/DuplicateArray.java
@@ -0,0 +1,9 @@
+package com.practice.array;
+
+public class DuplicateArray {
+    public int[] removeDuplicates(int[] arr) {
+        int[] result = new int[arr.length];
+        result[0] = arr[0];
+        return result;
+    }
+}
